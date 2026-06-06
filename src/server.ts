6da26b6775b5c4d572ebc200da1a41c1aeffaed6
Mohamed Medan <ma3025481@gmail.com>
2026-06-06 19:48:15 +0300

    feat: init ecommerce schema with 6 core models using prisma v7

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express, { type NextFunction, type Request, type Response } from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.send("Welcome to noon api");
