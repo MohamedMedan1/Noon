4a79c130060b19b2ee0bf31cc36878dbdc5b3591
Mohamed Medan <ma3025481@gmail.com>
2026-06-06 18:40:57 +0300

    chore: initial project setup with express 5, tsx and typescript 6

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express, { type NextFunction, type Request, type Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Welcome to noon api");
+});
+
+app.listen(PORT, () => {
+  console.log(`Noon App listening on port ${PORT}`)
+})
