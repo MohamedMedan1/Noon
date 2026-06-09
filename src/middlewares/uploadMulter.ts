751935ebbf6039454feb936af8c7825a2a667487
Mohamed Medan <ma3025481@gmail.com>
2026-06-09 19:42:29 +0300

    refactor(upload): create generic multer middleware factory

---
+++ src/middlewares/uploadMulter.ts
@@ -0,0 +1,25 @@
+import multer, { type FileFilterCallback } from "multer";
+import type { Request } from "express";
+
+const memoryStorage = multer.memoryStorage();
+
+const filterImages = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback,
+) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    // We will handle this error later
+  }
+};
+
+const upload = multer({ storage: memoryStorage, fileFilter: filterImages });
+export const uploadImage = (type: "single" | "fields") =>
+  type === "single"
+    ? upload.single("image")
+    : upload.fields([
+        { name: "image", maxCount: 1 },
+        { name: "subImages", maxCount: 4 },
+      ]);
