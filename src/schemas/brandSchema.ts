369cbe54307643770c087927e390999470f25725
Mohamed Medan <ma3025481@gmail.com>
2026-06-15 19:46:46 +0300

    feat(brand): update validation schema, fix route middleware

--- src/schemas/brandSchema.ts
+++ src/schemas/brandSchema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
+const ALLOWED_EXTENSIONS = ["png", "jpeg", "jfif", "jpg"];
+
 export const createBrandSchema = z.object({
   body: z.object({
     name: z
@@ -9,19 +11,33 @@ export const createBrandSchema = z.object({
       .min(2, "Brand name must be at least 2 characters")
       .max(50, "Brand name cannot be exceed 50 characters"),
   }),
-  file: z.object(
-    {
-      fieldname: z.string(),
-      originalname: z.string(),
-      mimetype: z.string().refine((val) => val.startsWith("image/"), {
-        message: "Only image files are allowed",
-      }),
-      buffer: z.instanceof(Buffer),
-    },
-    {
-      error: "Brand image file is required",
-    },
-  ),
+  file: z
+    .object(
+      {
+        fieldname: z.string(),
+        originalname: z.string(),
+        mimetype: z.string(),
+        buffer: z.instanceof(Buffer),
+      },
+      {
+        error: "Brand image file is required",
+      },
+    )
+    .refine(
+      (file) => {
+        if (file.mimetype.startsWith("image/")) return true;
+
+        const fileExtension = file.originalname.split(".").pop()?.toLowerCase();
+        return fileExtension
+          ? ALLOWED_EXTENSIONS.includes(fileExtension)
+          : false;
+      },
+      {
+        message:
+          "Invalid file type. Only JPG, JPEG, JFIF, and PNG images are allowed.",
+        path: ["mimetype"],
+      },
+    ),
 });
 
 export const updateBrandSchema = z.object({
@@ -34,17 +50,32 @@ export const updateBrandSchema = z.object({
       .max(50, "Brand name cannot be exceed 50 characters")
       .optional(),
   }),
-  file: z.object(
-    {
-      fieldname: z.string(),
-      originalname: z.string(),
-      mimetype: z.string().refine((val) => val.startsWith("image/"), {
-        message: "Only image files are allowed",
-      }),
-      buffer: z.instanceof(Buffer),
-    },
-    {
-      error: "Brand image file is required",
-    },
-  ).optional(),
+  file: z
+    .object(
+      {
+        fieldname: z.string(),
+        originalname: z.string(),
+        mimetype: z.string(),
+        buffer: z.instanceof(Buffer),
+      },
+      {
+        error: "Brand image file is required",
+      },
+    )
+    .refine(
+      (file) => {
+        if (file.mimetype.startsWith("image/")) return true;
+
+        const fileExtension = file.originalname.split(".").pop()?.toLowerCase();
+        return fileExtension
+          ? ALLOWED_EXTENSIONS.includes(fileExtension)
+          : false;
+      },
+      {
+        message:
+          "Invalid file type. Only JPG, JPEG, JFIF, and PNG images are allowed.",
+        path: ["mimetype"],
+      },
+    )
+    .optional(),
 });
